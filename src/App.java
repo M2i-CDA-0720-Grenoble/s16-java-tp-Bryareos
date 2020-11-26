b94e8f5b2584d9c73a21b281f6b688c8f4a71925
Bryareos <david-o@jhiki.fr>
2020-11-26 17:07:50 +0100

    Add instance of Masterming and while loop

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import Game.Mastermind;
+
 public class App
 {
     // Cette méthode est appelée automatiquement au lancement de l'application
@@ -5,7 +7,13 @@ public class App
         // Efface la console
         System.out.print("\033[H\033[2J");   
         System.out.flush();
-        
-        System.out.print("Hello, world!");
+
+        // Créer une nouvelle partie
+        Mastermind mastermind = new Mastermind();
+
+        while (mastermind.getIsRunning()) {
+            mastermind.update();
+        }
+        System.out.println("Goodbye !");
     }
 }
